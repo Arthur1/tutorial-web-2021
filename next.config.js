ce95ca88f27ada08dc6dc9a49fb47cffe37714b5
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2021-04-29 04:10:44 +0900

    webpack5, replace @import -> @use

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   basePath: isProd ? '/tutorial-web-2021' : '',
   trailingSlash: true,
+  future: {
+    webpack5: true,
+  },
 }

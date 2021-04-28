9829d2f853cc38245c39c30788ea2f813f08e037
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2021-04-29 04:58:54 +0900

    fix the bug of active header nav

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,6 +1,29 @@
 import React, { FC } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Navbar, Nav } from 'react-bootstrap'
+
+const HeaderNav: FC = () => {
+  const router = useRouter()
+  const activeKey = router.pathname
+
+  return (
+    <Nav className="mr-auto" activeKey={activeKey}>
+      <Link href="/" passHref>
+        <Nav.Link eventKey="/">Home</Nav.Link>
+      </Link>
+      <Link href="/courses/basic" passHref>
+        <Nav.Link eventKey="/courses/basic">WWW基礎</Nav.Link>
+      </Link>
+      <Link href="/courses/advanced" passHref>
+        <Nav.Link eventKey="/courses/advanced">WWW発展</Nav.Link>
+      </Link>
+      <Link href="/courses/security" passHref>
+        <Nav.Link eventKey="/courses/security">Webセキュリティ</Nav.Link>
+      </Link>
+    </Nav>
+  )
+}
 
 const Header: FC = () => (
   <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
@@ -9,20 +32,7 @@ const Header: FC = () => (
     </Link>
     <Navbar.Toggle aria-controls="navbar_nav" />
     <Navbar.Collapse id="navbar_nav">
-      <Nav className="mr-auto">
-        <Link href="/" passHref>
-          <Nav.Link>Home</Nav.Link>
-        </Link>
-        <Link href="/courses/basic" passHref>
-          <Nav.Link>WWW基礎</Nav.Link>
-        </Link>
-        <Link href="/courses/advanced" passHref>
-          <Nav.Link>WWW発展</Nav.Link>
-        </Link>
-        <Link href="/courses/security" passHref>
-          <Nav.Link>Webセキュリティ</Nav.Link>
-        </Link>
-      </Nav>
+      <HeaderNav />
     </Navbar.Collapse>
   </Navbar>
 )
